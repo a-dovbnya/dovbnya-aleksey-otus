76124accb4253367a59c48ae76906b36bcee022b
Алексей Довбня <custos11@gmail.com>
2021-10-14 11:20:14 +0300

    Homework angular part 1

---
+++ angular-1/src/app/default-data/mock-words.ts
@@ -0,0 +1,32 @@
+import { Word } from '../types/word'
+
+export const WORDS: Word[] = [
+    {
+        word: 'Say',
+        translate: 'говорить'
+    },
+    {
+        word: 'Use',
+        translate: 'использовать'
+    },
+    {
+        word: 'Make',
+        translate: 'делать'
+    },
+    {
+        word: 'See',
+        translate: 'видеть'
+    },
+    {
+        word: 'Look',
+        translate: 'смотреть'
+    },
+    {
+        word: 'Know',
+        translate: 'знать'
+    },
+    {
+        word: 'Take',
+        translate: 'брать'
+    }
+]
---
+++ angular-1/src/app/types/settings.ts
@@ -0,0 +1,5 @@
+export interface Settings {
+    lang: string,
+    wordInEx: number, // Количество слов в упражнении
+    timeForEx: number // Время упражнения
+}
